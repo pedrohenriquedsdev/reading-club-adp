68f1aa73fe03f684ebd1f7c8c00f41ece0f3f172
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-04-15 14:01:19 -0300

    Add project files.

---
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ClubeDaLeitura.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Clube da Leitura");
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("1 - Gerenciar caixas de revistas");
+                Console.WriteLine("2 - Gerenciar revistas");
+                Console.WriteLine("3 - Gerenciar amigos");
+                Console.WriteLine("4 - Gerenciar empréstimos");
+                Console.WriteLine("S - Sair");
+                Console.WriteLine("---------------------------------");
+                Console.Write("> ");
+                string? opcaoMenuPrincipal = Console.ReadLine()?.ToUpper();
+
+                if (opcaoMenuPrincipal == "S")
+                {
+                    Console.Clear();
+                    break;
+                }
+
+                while (true)
+                {
+                    string? opcaoMenuInterno = string.Empty;
+
+                    if (opcaoMenuPrincipal == "1")
+                    {
+
+                    }
+
+                    else if (opcaoMenuPrincipal == "2")
+                    {
+
+                    }
+
+                    else if (opcaoMenuPrincipal == "3")
+                    {
+
+                    }
+
+                    else if (opcaoMenuPrincipal == "4")
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}

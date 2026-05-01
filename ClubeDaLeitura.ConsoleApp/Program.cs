c27691a7d3d8722e01b81f953eb330678c92130d
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-05-01 16:32:26 -0300

    update: telas

--- ClubeDaLeitura.ConsoleApp/Program.cs
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -2,29 +2,28 @@
 using ClubeDaLeitura.ConsoleApp.Dominio;
 using ClubeDaLeitura.ConsoleApp.Infraestrutura;
 
-// 1. Instanciação de dependências
 RepositorioCaixa repositorioCaixa = new RepositorioCaixa();
 RepositorioRevista repositorioRevista = new RepositorioRevista();
-RepositorioAmigo repositorioAmigo = new RepositorioAmigo();
 
 TelaCaixa telaCaixa = new TelaCaixa(repositorioCaixa);
 TelaRevista telaRevista = new TelaRevista(repositorioRevista, repositorioCaixa);
-TelaAmigo telaAmigo = new TelaAmigo(repositorioAmigo);
 
-// 2. Criação de dados teste
+//DADOS TESTES
 Caixa caixa = new Caixa("Lançamentos", "Vermelho", 3);
 repositorioCaixa.Cadastrar(caixa);
 
 Revista revista = new Revista("Action Comics", 155, 1990, caixa);
 repositorioRevista.Cadastrar(revista);
 
-Amigo amigo = new Amigo("Joãozinho", "Dona Cleide", "49 98222-4353");
-repositorioAmigo.Cadastrar(amigo);
+//POLIMORFISMO
+EntidadeBase entidade = caixa;
 
-// 3. Loop principal
+entidade.AtualizarRegistro(new Caixa("Teste", "Vermelho", 5));
+
+
 while (true)
 {
-    Console.Clear();
+    //Console.Clear();
     Console.WriteLine("---------------------------------");
     Console.WriteLine("Clube da Leitura");
     Console.WriteLine("---------------------------------");
@@ -39,13 +38,13 @@ while (true)
 
     if (opcaoMenuPrincipal == "S")
     {
-        Console.Clear();
+        //Console.Clear();
         break;
     }
 
     while (true)
     {
-        string? opcaoMenuInterno;
+        string? opcaoMenuInterno = string.Empty;
 
         if (opcaoMenuPrincipal == "1") // Caixas
         {
@@ -53,7 +52,7 @@ while (true)
 
             if (opcaoMenuInterno == "S")
             {
-                Console.Clear();
+                //Console.Clear();
                 break;
             }
 
@@ -76,7 +75,7 @@ while (true)
 
             if (opcaoMenuInterno == "S")
             {
-                Console.Clear();
+                //Console.Clear();
                 break;
             }
 
@@ -93,27 +92,9 @@ while (true)
                 telaRevista.VisualizarTodos(deveExibirCabecalho: true);
         }
 
-        else if (opcaoMenuPrincipal == "3") // Amigos
+        else if (opcaoMenuPrincipal == "3")
         {
-            opcaoMenuInterno = telaAmigo.ObterOpcaoMenu();
 
-            if (opcaoMenuInterno == "S")
-            {
-                Console.Clear();
-                break;
-            }
-
-            if (opcaoMenuInterno == "1")
-                telaAmigo.Cadastrar();
-
-            else if (opcaoMenuInterno == "2")
-                telaAmigo.Editar();
-
-            else if (opcaoMenuInterno == "3")
-                telaAmigo.Excluir();
-
-            else if (opcaoMenuInterno == "4")
-                telaAmigo.VisualizarTodos(deveExibirCabecalho: true);
         }
 
         else if (opcaoMenuPrincipal == "4")

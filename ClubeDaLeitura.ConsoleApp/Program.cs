00dbf1439adeea86b8e2b4a12d7adf50994c4b51
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-05-03 02:57:53 -0300

    docs: merge branch 'v6' into main

--- ClubeDaLeitura.ConsoleApp/Program.cs
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -16,48 +16,14 @@ TelaPrincipal telaPrincipal = new TelaPrincipal
     repositorioEmprestimo
 );
 
-<<<<<<< HEAD
-// 2. Criação de dados teste
-
-Caixa caixa = new Caixa("Lançamentos", "Vermelho", 3);
-repositorioCaixa.Cadastrar(caixa);
-
-Revista revista = new Revista("Action Comics", 155, 1990, caixa);
-repositorioRevista.Cadastrar(revista);
-
-//POLIMORFISMO
-EntidadeBase entidade = caixa;
-
-entidade.AtualizarRegistro(new Caixa("Teste", "Vermelho", 5));
-
-Amigo amigo = new Amigo("Joãozinho", "Dona Cleide", "49 98222-4353");
-repositorioAmigo.Cadastrar(amigo);
-
-// 3. Loop principal
-while (true)
-{
-    //Console.Clear();
-    Console.WriteLine("---------------------------------");
-    Console.WriteLine("Clube da Leitura");
-    Console.WriteLine("---------------------------------");
-    Console.WriteLine("1 - Gerenciar caixas de revistas");
-    Console.WriteLine("2 - Gerenciar revistas");
-    Console.WriteLine("3 - Gerenciar amigos");
-    Console.WriteLine("4 - Gerenciar empréstimos");
-    Console.WriteLine("S - Sair");
-    Console.WriteLine("---------------------------------");
-    Console.Write("> ");
-    string? opcaoMenuPrincipal = Console.ReadLine()?.ToUpper();
-=======
 // 2. Loop do menu principal
 while (true)
 {
     ITela? telaSelecionada = telaPrincipal.ApresentarMenuOpcoesPrincipal();
->>>>>>> v6
 
     if (telaSelecionada == null)
     {
-        //Console.Clear();
+        Console.Clear();
         break;
     }
 
@@ -68,45 +34,12 @@ while (true)
 
         if (opcaoMenuInterno == "S")
         {
-<<<<<<< HEAD
-            opcaoMenuInterno = telaCaixa.ObterOpcaoMenu();
-
-            if (opcaoMenuInterno == "S")
-            {
-                //Console.Clear();
-                break;
-            }
-
-            if (opcaoMenuInterno == "1")
-                telaCaixa.Cadastrar();
-
-            else if (opcaoMenuInterno == "2")
-                telaCaixa.Editar();
-
-            else if (opcaoMenuInterno == "3")
-                telaCaixa.Excluir();
-
-            else if (opcaoMenuInterno == "4")
-                telaCaixa.VisualizarTodos(deveExibirCabecalho: true);
-=======
             Console.Clear();
             break;
->>>>>>> v6
         }
 
         if (telaSelecionada is TelaBase telaBase)
         {
-<<<<<<< HEAD
-            opcaoMenuInterno = telaRevista.ObterOpcaoMenu();
-
-            if (opcaoMenuInterno == "S")
-            {
-                //Console.Clear();
-                break;
-            }
-
-=======
->>>>>>> v6
             if (opcaoMenuInterno == "1")
                 telaBase.Cadastrar();
 
